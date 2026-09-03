20ee5df8a33eff3258c200f4848b3932b48dbc93
Yanlin Gong <yanlin.gong@cpaaustralia.com.au>
2026-09-03 11:25:33 +1000

    feat: add test smart banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Line Watch Melbourne",
   description: "Personal reminders for Melbourne train line disruptions.",
+  other: {
+    "apple-itunes-app":
+      "app-id=6752239608, affiliate-data=pt=2158661&ct=mobile_app_launch_promotion_cpa_landing_page_website&mt=8",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
